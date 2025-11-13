09f74aa2d4b857faef3a9c638fcb4e5bee30c4f2
brandaochico <brandao.chico24@gmail.com>
2025-11-13 15:34:32 -0300

    adjusting paint app

--- draw.py
+++ draw.py
@@ -21,7 +21,25 @@ def draw_on_canvas(event):
         fill= 'black'
     )
 
+def adjust_brush_size(event):
+    global brush_size
+    
+    if event.num == 4:
+        brush_size += 2
+    else:
+        brush_size -= 2
+
+    brush_size = max(0, min(brush_size,10))
+    print(event)
+
+
+def on_mousewheel(widget, func, prefix='Any'):
+    widget.bind(f'<{prefix}-Button-4>', func)
+    widget.bind(f'<{prefix}-Button-5>', func)
+
+
 brush_size = 4
 canvas.bind('<Motion>', draw_on_canvas)
+on_mousewheel(canvas, adjust_brush_size)
 
 window.mainloop()
